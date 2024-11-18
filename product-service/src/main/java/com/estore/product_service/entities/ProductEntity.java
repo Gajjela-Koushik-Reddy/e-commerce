8d179b4e1868a994216beaa7d6832d31cd37978b
Koushik Reddy Gajjala <kgajjala@csuchico.edu>
2024-11-18 10:12:51 -0800

    refactor: removed unused imports

--- product-service/src/main/java/com/estore/product_service/entities/ProductEntity.java
+++ product-service/src/main/java/com/estore/product_service/entities/ProductEntity.java
@@ -1,14 +1,12 @@
 package com.estore.product_service.entities;
 
 import java.util.List;
-import java.util.Set;
 import java.time.LocalDateTime;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import com.mongodb.lang.NonNull;
 
-import jakarta.persistence.Version;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
