154f112d3468e1bdeca0f77e120794b666273aa7
Koushik Reddy Gajjala <kgajjala@csuchico.edu>
2024-11-18 10:11:57 -0800

    refactor: simplified product entity by removing unnecessary feilds

--- product-service/src/main/java/com/estore/product_service/entities/ProductEntity.java
+++ product-service/src/main/java/com/estore/product_service/entities/ProductEntity.java
@@ -31,53 +31,12 @@ public class ProductEntity {
 
 	public String price;
 
-	public String discountedPrice;
-
-	public String skuCode;
-
-	public String upcCode;
-
 	public String description;
 
 	public String shortDescription;
 
-	private String brand;
-
-	private Integer stockQuantity;
-
-	private Integer lowStockThreshold;
-
-	private Boolean isActive;
-
-	private ProductStatus status;
-
-	private Set<String> tags;
-
 	private List<String> imageUrls;
-
-	// Physical attributes
-    private Double weight; // in kg
-    private Double length; // in cm
-    private Double width;  // in cm
-    private Double height; // in cm
-
-	// SEO fields
-    private String metaTitle;
-    private String metaDescription;
-    private String metaKeywords;
 
 	private LocalDateTime createdAt;
 	private LocalDateTime lastModifiedAt;
-
-	@Version
-	private Long version;
-
-	// Enum for product status
-    public enum ProductStatus {
-        DRAFT,
-        ACTIVE,
-        INACTIVE,
-        OUT_OF_STOCK,
-        DISCONTINUED
-    }
 }
