07a487efd9d355f9ac55fd260a42a087b3fc8b8d
Koushik Reddy Gajjala <kgajjala@csuchico.edu>
2024-11-25 13:40:16 -0800

    feat: added create product with validation and transctional support

--- product-service/src/main/java/com/estore/product_service/service/impl/ProductServiceImpl.java
+++ product-service/src/main/java/com/estore/product_service/service/impl/ProductServiceImpl.java
@@ -2,8 +2,10 @@ package com.estore.product_service.service.impl;
 
 import java.io.IOException;
 import java.security.InvalidKeyException;
+import java.security.InvalidParameterException;
 import java.security.NoSuchAlgorithmException;
-import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
 import java.util.List;
 import java.util.UUID;
 
@@ -14,6 +16,8 @@ import org.springframework.web.multipart.MultipartFile;
 import com.estore.product_service.entities.ProductEntity;
 import com.estore.product_service.repository.ProductRepository;
 
+import jakarta.transaction.Transactional;
+
 @Service
 public class ProductServiceImpl {
 
@@ -23,26 +27,44 @@ public class ProductServiceImpl {
     @Autowired
     ImageServiceImpl imageServiceImpl;
 
-    public boolean addProduct(MultipartFile[] images)
+    @Transactional
+    public void createProduct(
+            String catrgoryId,
+            String productName,
+            String price,
+            String description,
+            String shortDescription,
+            MultipartFile[] images)
             throws IOException, InvalidKeyException, NoSuchAlgorithmException {
+
+        // Validate inputs
+        if(catrgoryId.isEmpty() || catrgoryId == null)
+                throw new InvalidParameterException("Category Id cannot be empty or null");
+        if(productName.isEmpty() || productName == null)
+                throw new InvalidParameterException("product name cannot be empty or null");
+        if(price.isEmpty() || price == null)
+                throw new InvalidParameterException("price cannot be empty or null");
+        if(description.isEmpty() || description == null)
+                throw new InvalidParameterException("description cannot be empty");
+        if(shortDescription.isEmpty() || shortDescription == null)
+                throw new InvalidParameterException("short descriptiion cannot be empty");
 
         ProductEntity newProduct = new ProductEntity();
 
         newProduct.setProductId(UUID.randomUUID().toString());
-        newProduct.setCategoryId(UUID.randomUUID().toString());
-        newProduct.setName("something");
-        newProduct.setPrice("1000");
-        newProduct.setDescription("this is the description of the product");
-        newProduct.setShortDescription("this is short des");
+        newProduct.setCategoryId(catrgoryId);
+        newProduct.setName(productName);
+        newProduct.setPrice(price);
+        newProduct.setDescription(description);
+        newProduct.setShortDescription(shortDescription);
+        newProduct.setCreatedAt(ZonedDateTime.now(ZoneId.of("UTC")));
+
         try {
             List<String> imageStrings = imageServiceImpl.uploadImages(images);
             newProduct.setImageUrls(imageStrings);
+            productRepository.save(newProduct);
         } catch (Exception e) {
-            throw new RuntimeException(e.getMessage());
+            throw new RuntimeException("Error creating the product" + e.getMessage());
         }
-
-        newProduct.setCreatedAt(LocalDateTime.now());
-        productRepository.save(newProduct);
-        return false;
     }
 }
